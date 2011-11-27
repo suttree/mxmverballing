1bd99084d616bfa7dfc079b58434771aee2628d2
Duncan Gough <duncan@suttree.com>
2011-11-27 20:50:03 +0000

    Frist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mxmverballing::Application.config.secret_token = '7e771b11a9b666e7b03c2bd243f5ccd702fd3eb9583c2900e5753ec748abf4816d6005e81f23f140d43339eadc46d8fbe3563dc24276414f0f1a85eec8fe9078'
